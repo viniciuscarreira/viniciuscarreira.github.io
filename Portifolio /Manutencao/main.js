3020f9b018278fc5cd6988c20e49ffc669bca125
Vinicius Carreira <101435617+viniciuscarreira@users.noreply.github.com>
2022-11-18 16:48:07 -0300

    Add files via upload

---
+++ Portifolio /Manutencao/main.js
@@ -0,0 +1,14 @@
+// ERROR 404 ALERTA
+
+alert("ERROR 404 | NAO ME INSPECIONE");
+
+// ERROR 404 CONSOLE
+console.log("E");
+console.log("R");
+console.log("R");
+console.log("O");
+console.log("R");
+console.log("-");
+console.log("4");
+console.log("0");
+console.log("4");
